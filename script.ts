08d594f08d7f8de234d4d1f95ef69df8d2235866
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-08-06 02:13:17 +0100

    update prisma client

--- script.ts
+++ script.ts
@@ -2,20 +2,15 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    const user1 = await prisma.user.create({ 
-        data:{
-            name: "fecha"
-        }
-    })
-    const user2 = await prisma.user.create({ 
-        data:{
-            name: "fecha2"
+    await prisma.user.create({
+        data: {
+            
         }
     })
     // console.log(user1);
-    console.log(user2);
-    const users = await prisma.user.findMany()
-    console.log(users);
+    // console.log(user2);
+    // const users = await prisma.user.findMany()
+    // console.log(users);
 }
 
 main()
